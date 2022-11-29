a178f176c3748cb490236127fc80419634e61598
yudhatp <yudhatp@gmail.com>
2022-11-29 13:09:14 +0700

    add more function

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Laravel Helpers
 
-Collection of helper functions for laravel.
+Collection of (9) helper functions for laravel.
 
 ## Installation
 
@@ -16,12 +16,25 @@ composer require yudhatp/laravel-helpers
 use Yudhatp\Helpers\Helpers;
 Helpers::terbilang("2000"); //Dua Ribu
 Helpers::indonesianMonthName("2022-11-28"); //28 November 2022
-Helpers::indonesianShortMonthName("2022-11-28"); //28 Nov 2022
+Helpers::indonesianShortMonthName("2022-11-28"); //28 Nov 22
+Helpers::getAgeIndonesian("1945-08-17"); //77 Tahun, 3 Bulan, 12 Hari
+Helpers::isWeekend("2022-11-29"); //false
+Helpers::generatePassword(8); //EPp218kR
+Helpers::indonesianFormatDecimal("2.100,00"); //2100.00
+Helpers::addDays("2022-11-28",2); //2022-11-30
+Helpers::indonesianPoliceNumberformat("B123XYZ"); //B 123 XYZ
+
+//blade
+{{ (new \Yudhatp\Helpers\Helpers)->indonesianMonthName("2022-11-28") }}
 ```
 
 ## Changelog
 
 Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Credits
+
+- [@matriphe](https://https://gist.github.com/matriphe/3103ec578ec556bad5047b378520f070)
 
 
 ## License
--- src/Helpers.php
+++ src/Helpers.php
@@ -2,8 +2,65 @@
 
 namespace Yudhatp\Helpers;
 
+use Carbon\Carbon;
+use DateTime;
+
 class Helpers
 {
+
+	//source from https://https://gist.github.com/matriphe/3103ec578ec556bad5047b378520f070
+	public static function indonesianPoliceNumberformat($string)
+	{
+	  $string = strtoupper(trim($string));  
+	
+	  $pattern = '/^([A-Z]{1,3})(\s|-)*([1-9][0-9]{0,3})(\s|-)*([A-Z]{0,3}|[1-9][0-9]{1,2})$/i';
+	  if (preg_match($pattern, $string)) {
+		return trim(strtoupper(preg_replace($pattern, '$1 $3 $5', $string)));
+	  }
+	
+	  // militer dan kepolisian
+	  $pattern = '/^([0-9]{1,5})(\s|-)*([0-9]{2}|[IVX]{1,5})*/';
+	  if (preg_match($pattern, $string)) {
+		return trim(strtoupper(preg_replace($pattern, '$1-$3', $string)));
+	  }
+	  
+	  return null;
+	}
+	
+	public static function addDays($date, $day){
+		return Carbon::createFromFormat('Y-m-d',$date)->addDays($day)->toDateString();
+	}
+	
+	public static function indonesianFormatDecimal($number){
+        $temp = str_replace('.','',$number);
+        $temp = str_replace(',','.',$temp);
+        return $temp;
+    }
+
+	public static function generatePassword($char) {
+        $tmp = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+  		return substr(str_shuffle($tmp), 0, $char);
+    }
+
+	public static function isWeekend($date){
+		$day = date('D', strtotime($date));
+        if($day == "Sat" || $day == "Sun"){
+            return true;
+        }else{
+			return false;
+		}
+	}
+
+	public static function getAgeIndonesian($birthdate){
+		$tmp     	= new DateTime($birthdate);
+        $curdate   	= new DateTime();
+        $diff       = $tmp->diff($curdate);
+        $year       = $diff->y;
+        $month      = $diff->m;
+        $day        = $diff->d;
+		return $year." Tahun, ".$month." Bulan, ".$day." Hari";
+	}
+
     public static function indonesianMonthName($date) {
         $date = date_create($date);
         $month = date_format($date,"n");
@@ -29,23 +86,23 @@ class Helpers
 		if ($nilai < 12) {
 			$temp = " ". $huruf[$nilai];
 		} else if ($nilai <20) {
-			$temp = $this->terbilang($nilai - 10). " Belas";
+			$temp = static::terbilang($nilai - 10). " Belas";
 		} else if ($nilai < 100) {
-			$temp = $this->terbilang($nilai/10)." Puluh". $this->terbilang($nilai % 10);
+			$temp = static::terbilang($nilai/10)." Puluh". static::terbilang($nilai % 10);
 		} else if ($nilai < 200) {
-			$temp = " Seratus" . $this->terbilang($nilai - 100);
+			$temp = " Seratus" . static::terbilang($nilai - 100);
 		} else if ($nilai < 1000) {
-			$temp = $this->terbilang($nilai/100) . " Ratus" . $this->terbilang($nilai % 100);
+			$temp = static::terbilang($nilai/100) . " Ratus" . static::terbilang($nilai % 100);
 		} else if ($nilai < 2000) {
-			$temp = " Seribu" . $this->terbilang($nilai - 1000);
+			$temp = " Seribu" . static::terbilang($nilai - 1000);
 		} else if ($nilai < 1000000) {
-			$temp = $this->terbilang($nilai/1000) . " Ribu" . $this->terbilang($nilai % 1000);
+			$temp = static::terbilang($nilai/1000) . " Ribu" . static::terbilang($nilai % 1000);
 		} else if ($nilai < 1000000000) {
-			$temp = $this->terbilang($nilai/1000000) . " Juta" . $this->terbilang($nilai % 1000000);
+			$temp = static::terbilang($nilai/1000000) . " Juta" . static::terbilang($nilai % 1000000);
 		} else if ($nilai < 1000000000000) {
-			$temp = $this->terbilang($nilai/1000000000) . " Milyar" . $this->terbilang(fmod($nilai,1000000000));
+			$temp = static::terbilang($nilai/1000000000) . " Milyar" . static::terbilang(fmod($nilai,1000000000));
 		} else if ($nilai < 1000000000000000) {
-			$temp = $this->terbilang($nilai/1000000000000) . " Trilyun" . $this->terbilang(fmod($nilai,1000000000000));
+			$temp = static::terbilang($nilai/1000000000000) . " Trilyun" . static::terbilang(fmod($nilai,1000000000000));
 		}     
 		return $temp;
 	}
